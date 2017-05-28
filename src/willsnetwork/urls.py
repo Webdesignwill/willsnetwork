afcad97c195e36f44e92e995fc6399c2f7754943
Michael van de Waeter <mvdwaeter@gmail.com>
2017-05-28 14:07:29 +0200

    Added example HTML

--- src/willsnetwork/urls.py
+++ src/willsnetwork/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
-
+from willsnetwork.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('rest_api.urls', namespace='rest_api')),
     url(r'^favicon\.ico$', RedirectView.as_view(
