35bbb438e248fa0f070309b6251ec91cb87fee48
Michael van de Waeter <mvdwaeter@gmail.com>
2017-05-28 13:24:59 +0200

    Fixed deploy script

--- deploy/deploy_production.sh
+++ deploy/deploy_production.sh
@@ -2,9 +2,7 @@ read -p "Are you sure you want to deploy to Heroku [production] [y/n]? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    cd ..
     git push heroku master
-    heroku run python src/manage.py migrate
-    heroku open
-    cd deploy
+    heroku run --app willsnetwork python src/manage.py migrate
+    heroku open --app willsnetwork
 fi
